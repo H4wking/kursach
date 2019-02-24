6af7b7445e1f578d6463402fa58cf774d104e4b3
H4wking <funnyman20062001@gmail.com>
2019-02-24 14:09:34 +0200

    xml -> json

--- api_shell.py
+++ api_shell.py
@@ -1,4 +1,6 @@
 import http.client
+import xmltodict
+import json
 
 conn = http.client.HTTPSConnection("api.sportradar.us")
 
@@ -7,4 +9,6 @@ conn.request("GET", "/tennis-t2/en/players/sr:competitor:18111/versus/sr:competi
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+xml_data = data.decode("utf-8")
+json_data = xmltodict.parse(xml_data)
+print(json.dumps(json_data, indent=2))
